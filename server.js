52e162951f34a0febce27121b073bcd524cdf6d2
AbacateNuclear <rafagois09@gmail.com>
2026-06-12 14:49:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -26,4 +26,8 @@ app.post("/corrida",(req,res)=>
     res.send("OK");
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
